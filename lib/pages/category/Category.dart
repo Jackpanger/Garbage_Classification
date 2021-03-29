601c635639b8a518c95c88b7d88f2db569f9250a
Jackpanger <442924580@qq.com>
2021-03-29 15:00:30 +0800

    Category list upload

--- lib/pages/category/Category.dart
+++ lib/pages/category/Category.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:garbage_classification/global_config.dart';
 
-class CategoryPage extends StatefulWidget{
+class CategoryPage extends StatefulWidget {
   @override
   _CategoryState createState() => _CategoryState();
-
 }
-class _CategoryState extends State<CategoryPage>{
+
+class _CategoryState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: GlobalConfig.themeData,
       home: Scaffold(
           appBar: AppBar(title: Text("Category"), centerTitle: true),
-      ),
+          body: new ListView(
+            children: <Widget>[
+              new ListTile(
+                  leading: new Icon(Icons.accessibility_sharp),
+                  title: new Text('Dry garbage')),
+              new ListTile(
+                  leading: new Icon(Icons.accessible_forward_sharp),
+                  title: new Text('Wet garbage')),
+              new ListTile(
+                  leading: new Icon(Icons.accessible_sharp),
+                  title: new Text('Kitchen garbage')),
+              new ListTile(
+                leading: new Icon(Icons.not_accessible_sharp),
+                title: new Text('Hazardous garbage'),
+              )
+            ],
+          )),
     );
   }
-
-
 }
