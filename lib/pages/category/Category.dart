25546b04ace8186407a220ea6dd593359f38aaed
Jackpanger <442924580@qq.com>
2021-04-16 14:57:54 +0800

    basic UI

--- lib/pages/category/Category.dart
+++ lib/pages/category/Category.dart
@@ -1,35 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:garbage_classification/global_config.dart';
+import 'res/form1.dart';
+import 'res/form2.dart';
+import 'res/form3.dart';
+import 'res/form4.dart';
 
-class CategoryPage extends StatefulWidget {
+
+class CategoryPage extends StatefulWidget{
   @override
   _CategoryState createState() => _CategoryState();
+
 }
 
-class _CategoryState extends State<CategoryPage> {
+
+class _CategoryState extends State<CategoryPage>{
+
+
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: GlobalConfig.themeData,
-      home: Scaffold(
-          appBar: AppBar(title: Text("Category"), centerTitle: true),
-          body: new ListView(
-            children: <Widget>[
-              new ListTile(
-                  leading: new Icon(Icons.accessibility_sharp),
-                  title: new Text('Dry garbage')),
-              new ListTile(
-                  leading: new Icon(Icons.accessible_forward_sharp),
-                  title: new Text('Wet garbage')),
-              new ListTile(
-                  leading: new Icon(Icons.accessible_sharp),
-                  title: new Text('Kitchen garbage')),
-              new ListTile(
-                leading: new Icon(Icons.not_accessible_sharp),
-                title: new Text('Hazardous garbage'),
-              )
-            ],
-          )),
+
+    return DefaultTabController(length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: GlobalConfig.cardBackgroundColor,
+            bottom: TabBar(
+          //    backgroundColor: GlobalConfig.cardBackgroundColor,
+              tabs: <Widget>[
+                Tab(child: Text(
+                  "干垃圾",
+                 // textAlign: TextAlign.left,
+                  style: TextStyle( color: GlobalConfig.fontColor),
+                ),),
+                Tab(child: Text(
+                  "湿垃圾",
+                  // textAlign: TextAlign.left,
+                  style: TextStyle(color: GlobalConfig.fontColor),
+                ),), Tab(child: Text(
+                  "可回收垃圾",
+                  // textAlign: TextAlign.left,
+                  style: TextStyle( color: GlobalConfig.fontColor),
+                ),), Tab(child: Text(
+                  "其他垃圾",
+                  // textAlign: TextAlign.left,
+                  style: TextStyle(color: GlobalConfig.fontColor),
+                ),),
+              ],
+            ),
+          ),
+
+          body: TabBarView(
+          children: <Widget>[
+            ListView.builder(itemCount: form1.length
+              ,itemBuilder: (context,index){
+                return ListTile(
+                  title: Text(form1[index]["title"]),
+                  leading: Image.network(form1[index]['imageUrl']),
+
+                );
+                }
+            ),
+
+            ListView.builder(itemCount: form2.length
+                ,itemBuilder: (context,index){
+                  return ListTile(
+                    title: Text(form2[index]["title"]),
+                  );
+                }
+            ),
+
+            ListView.builder(itemCount: form3.length
+                ,itemBuilder: (context,index){
+                  return ListTile(
+                    title: Text(form3[index]["title"]),
+                  );
+                }
+            ),
+
+            ListView.builder(itemCount: form4.length
+                ,itemBuilder: (context,index){
+                  return ListTile(
+                    title: Text(form3[index]["title"]),
+                  );
+                }
+            ),
+
+
+
+
+
+          ]
+
+
+
+          )
+
+        ,
+        )
     );
+
   }
+
+
 }
